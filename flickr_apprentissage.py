a9d780b3a27ce77b66bbf492165132eeafd7b79a
Karim <ozerioss@gmail.com>
2018-09-07 15:29:45 +0200

    Adds max nbr of pictures for homeCountry learning

--- flickr_apprentissage.py
+++ flickr_apprentissage.py
@@ -5,12 +5,12 @@ import numpy as np
 import matplotlib.dates as mdates
 
 
-query_flickr = "SELECT gadm2.gadm2.name_0, count(name_0) as nbr\
-                FROM Flickr\
-                JOIN gadm2.gadm2 ON gadm2.gadm2.gid = Flickr.shape_gid\
-                WHERE owner_id = '58225608@N06'\
-                GROUP BY name_0\
-                ORDER BY nbr desc;\
+query_flickr = "SELECT gadm2.gadm2.name_0, count(name_0) as nbr             \
+                FROM Flickr                                                 \
+                JOIN gadm2.gadm2 ON gadm2.gadm2.gid = Flickr.shape_gid      \
+                WHERE owner_id = %s                                         \
+                GROUP BY name_0                                             \
+                ORDER BY nbr desc;                                          \
             "
 
 query_random_users_Flickr = "SELECT owner_id FROM Flickr\
@@ -41,10 +41,35 @@ def GetRandomFlickrUsers():
         print("Error : {}".format(mdb.Error.args))
 
 
+def GetMaxCountry():
+    with open("users_Flickr.txt", encoding = "utf8") as infile:
+        content = infile.read().splitlines()
+        for user in content:
+            print(user)
+            try:
+                cursor.execute(query_flickr, user)
+                result = cursor.fetchall()
+                print(result)
+                for tmp in result:
+                    print(tmp[0], tmp[1])
+            except mdb.Error:
+                print("Error : {}".format(mdb.Error.args))
+                
+
+
+def testMethod():
+    try:
+        cursor.execute(query_flickr, '60764770@N08')
+        result = cursor.fetchall()
+        print(result)
+        for tmp in result:
+            print(tmp[0], tmp[1])
+    except mdr.Error:
+        print("Error : {}".format(mdb.Error.args))
 
 if(__name__ == "__main__"):
     cursor = connection.cursor()
-    GetRandomFlickrUsers()
+    GetMaxCountry()
     print("Done ! ")
     if connection:
         connection.close()
