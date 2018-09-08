9a14af2d7b9a2cd38567602bec2bc701f0afa853
Karim <ozerioss@gmail.com>
2018-09-08 18:41:35 +0200

    adds flickr insert

--- flickr_apprentissage.py
+++ flickr_apprentissage.py
@@ -6,18 +6,19 @@ import matplotlib.dates as mdates
 
 
 query_flickr = "SELECT gadm2.gadm2.name_0, count(name_0) as nbr             \
-                FROM Flickr                                                 \
-                JOIN gadm2.gadm2 ON gadm2.gadm2.gid = Flickr.shape_gid      \
+                FROM Flickr_F                                                 \
+                JOIN gadm2.gadm2 ON gadm2.gadm2.gid = Flickr_F.shape_gid      \
                 WHERE owner_id = %s                                         \
                 GROUP BY name_0                                             \
                 ORDER BY nbr desc;                                          \
             "
 
-query_random_users_Flickr = "SELECT owner_id FROM Flickr\
-                                ORDER BY rand() LIMIT 1000000;\
+query_random_users_Flickr = "SELECT owner_id FROM Flickr_F\
+                                ORDER BY rand() LIMIT 500000;\
                             "
 
 
+query_insert_Flickr = "INSERT INTO Flickr_F (topCountry, secondCountry) VALUES (%s, %s);"
 
 connection = mdb.connect(host="127.0.0.1", 
                         user="KarimKidiss", 
@@ -28,10 +29,13 @@ connection = mdb.connect(host="127.0.0.1",
                         autocommit=True
                     )
 
+
 def GetRandomFlickrUsers():
     try:
+        print("querying random users")
         cursor.execute(query_random_users_Flickr)
         result = cursor.fetchall()
+        print("done")
         newLine = open("users_Flickr.txt", "w", encoding = "utf8")
         for tmp in result:
             print(tmp[0])
@@ -41,31 +45,41 @@ def GetRandomFlickrUsers():
         print("Error : {}".format(mdb.Error.args))
 
 
-def GetMaxCountry():
+
+
+def getListCountries(user):
+    top2Countries = []
+    try:
+        print("Querying .. Top countries visited for {}".format(user))
+        cursor.execute(query_flickr, user)
+        print("OK")
+        result = cursor.fetchall()
+
+        for tmp in result:
+            listCountries.append(tmp[0])
+            listValues.append(tmp[1])
+
+    except mdb.Error:
+        print("Exception {} : {} ".format(mdb.Error.args[0], mdb.Error.args[1]))
+        sys.exit(1)
+    return listCountries, listValues
+
+def insertFlickr():
     with open("users_Flickr.txt", encoding = "utf8") as infile:
         content = infile.read().splitlines()
         for user in content:
-            print(user)
+            listCountries, listValues = getListCountries(user)
             try:
-                cursor.execute(query_flickr, user)
-                result = cursor.fetchall()
-                print(result)
-                for tmp in result:
-                    print(tmp[0], tmp[1])
+                cursor.execute(insertFlickr, (listCountries[0], listCountries[1]))
+                print("inserted : {} and {}".format(listCountries[0], listCountries[1]))
             except mdb.Error:
-                print("Error : {}".format(mdb.Error.args))
+                print("Exception {} : {} ".format(mdb.Error.args[0], mdb.Error.args[1]))
+                sys.exit(1)
+
                 
 
 
-def testMethod():
-    try:
-        cursor.execute(query_flickr, '60764770@N08')
-        result = cursor.fetchall()
-        print(result)
-        for tmp in result:
-            print(tmp[0], tmp[1])
-    except mdr.Error:
-        print("Error : {}".format(mdb.Error.args))
+
 
 if(__name__ == "__main__"):
     cursor = connection.cursor()
